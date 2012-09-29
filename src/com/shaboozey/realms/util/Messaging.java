5a2d3056e9b1f12060072bf6f04f119f566fa04c
Patrick Campbell <patters.campbell@gmail.com>
2012-09-29 13:59:20 +0100

    Added console logging for any response

--- src/com/shaboozey/realms/util/Messaging.java
+++ src/com/shaboozey/realms/util/Messaging.java
@@ -1,7 +1,12 @@
 package com.shaboozey.realms.util;
 
+import java.util.logging.Level;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.shaboozey.realms.ShaboozeyRealms;
 
 public class Messaging {
 
@@ -9,11 +14,20 @@ public class Messaging {
 
 	public static void message(CommandSender sender, String message)
 	{
-		sender.sendMessage(prefix + ChatColor.WHITE + message);
+		if(sender instanceof Player)
+		{
+			sender.sendMessage(prefix + ChatColor.WHITE + message);
+		}
+		ShaboozeyRealms.getPlugin().getLogger().log(Level.INFO, message + " by " + sender.getName());
+		
 	}
 	
 	public static void error(CommandSender sender, String message)
 	{
-		sender.sendMessage(prefix + ChatColor.RED + "Error! " + message);
+		if(sender instanceof Player)
+		{
+			sender.sendMessage(prefix + ChatColor.RED + "Error! " + message);
+		}
+		ShaboozeyRealms.getPlugin().getLogger().log(Level.INFO, message + " by " + sender.getName());
 	}
 }
